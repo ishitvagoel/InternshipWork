9ae556002d1481fd859b342e6d8ad237c8f84bf7
Ishitva Goel <ishitva.goel@yahoo.co.in>
2013-02-10 15:49:17 +0530

    Update status.php

--- status.php
+++ status.php
@@ -1,7 +1,12 @@
 <?php
 	session_start();
-	if(!isset($_SESSION['id2'])){var_dump($_SESSION['id']); header("Location:Homepage.php");}
+	
+	if(!isset($_SESSION['id2'])){
+		var_dump($_SESSION['id']); header("Location:Homepage.php");
+	}
+	
 	$con=mysql_connect("localhost","root","");
+	
 	mysql_select_db("db2",$con);
 	
 	$sql='UPDATE data SET SUpdate="'.$_REQUEST['status'].'" WHERE Sno='.$_SESSION['Sno'] ;
