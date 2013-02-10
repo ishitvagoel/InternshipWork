0bfa99e7c79c18e11e5ccfc7be93e3c56b52a169
Ishitva Goel <ishitva.goel@yahoo.co.in>
2013-02-10 15:50:40 +0530

    Update remove.php

--- remove.php
+++ remove.php
@@ -1,6 +1,10 @@
 <?php
 	session_start();
-	if(!isset($_SESSION['id2'])){var_dump($_SESSION['id']); header("Location:Homepage.php");}
+	
+	if(!isset($_SESSION['id2'])){
+		var_dump($_SESSION['id']); 
+		header("Location:Homepage.php");
+	}
 	
 	$con2=mysql_connect("localhost","root","");
 	mysql_select_db("db2",$con2);
@@ -8,11 +12,11 @@
 	$sql='SELECT FriendSno FROM friends WHERE UserSno='.$_SESSION['Sno'] ;
 	$data=mysql_query($sql);
 	while($rem=mysql_fetch_array($data)){
+		
 		if(isset($_POST[$rem['FriendSno']])){
 			$sql2='DELETE FROM friends WHERE UserSno='.$_SESSION['Sno'].' AND FriendSno='.$rem['FriendSno'].';';
-			
 			mysql_query($sql2);
 		}
 	}
-header("Location:fedit.php");
+	header("Location:fedit.php");
 ?>	
